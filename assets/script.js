95fa0ce4fe0e99cc047809b38dabaf415601aa8a
Noah Fajarda <noahfajarda@gmail.com>
2022-08-11 10:03:18 -0700

    added JS file to add logic to site

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// initialize variable "type <string>"
+var topics = "HTML, CSS, Git, Javascript";
+
+// console.log == output message to web console
+    // 'console' == test environment
+    // '.log()' == output to console
+    // methods ('()') == execution instructions for computer
+
+    // mostly used to test code
+    // can be viewed in 'console' tab of webpage 'inspect'
+console.log(topics);
